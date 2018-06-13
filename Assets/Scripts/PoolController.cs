63ddb8fb76f586dfc99f531a0f704ce8fda2efd7
jyotiroyuff <cse.roy.jyoti@gmail.com>
2018-06-13 11:56:35 +0600

    Initial Commite!

---
+++ Assets/Scripts/PoolController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolController : MonoBehaviour {
+
+    public Rigidbody2D StickBody; //reference to the rigidbody2d of the stick
+    public Rigidbody2D BusketBody;//reference to the rigidbody2d of the busket
+    public Rigidbody2D ThroughObjectBody; //reference to the rigidbody2d to the through object
+
+    public int TapCount; //counts the taps
+
+    private void Start()
+    {
+        TapCount = 0;// initialize the tapcount
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+#if UNITY_EDITOR 
+        //tap funtions on display
+        if (Input.GetMouseButtonDown(0)) //detect mouse click 
+        {
+            Debug.Log("Clicked");
+            TapCount++; //count clicks
+            if(TapCount == 1) //for first click
+            {
+                StickBody.bodyType = RigidbodyType2D.Dynamic; //makes the rigidbody of the stick active
+
+            }else if(TapCount == 2) //for second click
+            {
+                //through the basket and through object
+                Destroy(BusketBody.GetComponent<HingeJoint2D>()); //destroy busket joint
+                Destroy(BusketBody.GetComponent<DistanceJoint2D>());//destroy busket joint
+                Destroy(ThroughObjectBody.GetComponent<FixedJoint2D>());//destroy through object joint
+                BusketBody.velocity *= 1.5f;//modify busket velocity
+                ThroughObjectBody.velocity *= 3f;//modify busket velocity
+                ThroughObjectBody.transform.parent = null;//clear parent
+                ThroughObjectBody.GetComponent<ParticleSystem>().Play();//start playing particles
+            }
+        }
+#elif UNITY_ANDROID || UNITY_IOS
+
+
+      if (Input.touchCount>0) //detect mouse click 
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.Log("Clicked");
+                TapCount++; //count clicks
+            }
+            else return;
+           
+            if (TapCount == 1) //for first click
+            {
+                StickBody.bodyType = RigidbodyType2D.Dynamic; //makes the rigidbody of the stick active
+
+            }
+            else if (TapCount == 2) //for second click
+            {
+                //through the basket and through object
+                Destroy(BusketBody.GetComponent<HingeJoint2D>());//destroy busket joint
+                Destroy(BusketBody.GetComponent<DistanceJoint2D>());//destroy busket joint
+                Destroy(ThroughObjectBody.GetComponent<FixedJoint2D>());//destroy through object joint
+                BusketBody.velocity *= 1.5f; //modify busket velocity
+                ThroughObjectBody.velocity *= 3f; //modify through object velocity
+                ThroughObjectBody.transform.parent = null;//clear parent
+                ThroughObjectBody.GetComponent<ParticleSystem>().Play();//start playing particles
+
+            }
+        }
+
+
+#endif
+
+
+
+
+
+    }
+
+
+
+}
